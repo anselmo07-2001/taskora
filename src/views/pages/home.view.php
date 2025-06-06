edcf08ac3bffba954febb31c191d0023055f1fae
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-06-06 04:01:44 -0700

    implemented conditionally render subpages base on user role

--- src/views/pages/home.view.php
+++ src/views/pages/home.view.php
@@ -5,53 +5,127 @@
     
         <div class="container mt-5">
             <div class="row mb-4">
-                <div class="col-12 col-md-6 col-lg-4 mb-3">
-             <div class="card custom-card border rounded shadow p-3 lift-hover">
-                        <div class="card-body d-flex flex-column align-items-center">
-                            <img src="./public/images/scope.png" class="w-50 mb-2"/>
-                            <p class="card-title fs-5">Projects</p>
-                        </div>
-                        <a href="showProjects.html" class="btn custom-primary-btn">Open</a>
-                    </div>
-                </div>
-                <div class="col-12 col-md-6 col-lg-4 mb-3">
-                    <div class="card custom-card border rounded shadow p-3 lift-hover">
-                        <div class="card-body d-flex flex-column align-items-center">
-                            <img src="./public/images/choosing.png" class="w-50 mb-2"/>
-                             <p class="card-title fs-5">Members</p>
-                        </div>
-                        <a href="#" class="btn custom-primary-btn">Open</a>
-                    </div>
-                </div>
-                <div class="col-12 col-md-6 col-lg-4 mb-3">
-                    <div class="card custom-card border rounded shadow p-3 lift-hover">
-                        <div class="card-body d-flex flex-column align-items-center">
-                            <img src="./public/images/documentation.png" class="w-50 mb-2"/>
-                             <p class="card-title fs-5">Tasks</p>
-                        </div>
-                        <a href="#" class="btn custom-primary-btn">Open</a>
-                    </div>
-                </div>
 
-            
-                <div class="col-12 col-md-6 col-lg-4 mb-3">
-                    <div class="card custom-card border rounded shadow p-3 lift-hover">
-                        <div class="card-body d-flex flex-column align-items-center">
-                            <img src="./public/images/clipboard.png" class="w-50 mb-2"/>
-                             <p class="card-title fs-5">Create Projects</p>
+                <?php if ($user["role"] === "member"): ?>
+                    <div class="col-12 col-md-6 col-lg-4 mb-3">
+                        <div class="card custom-card border rounded shadow p-3 lift-hover">
+                            <div class="card-body d-flex flex-column align-items-center">
+                                <img src="./public/images/scope.png" class="w-50 mb-2"/>
+                                <p class="card-title fs-5">Projects</p>
+                            </div>
+                            <a href="showProjects.html" class="btn custom-primary-btn">Open</a>
                         </div>
-                        <a href="createProject.html" class="btn custom-primary-btn">Open</a>
                     </div>
-                </div>
-                <div class="col-12 col-md-6 col-lg-4 mb-3">
-                    <div class="card custom-card border rounded shadow p-3 lift-hover">
-                        <div class="card-body d-flex flex-column align-items-center">
-                            <img src="./public/images/add-friend.png" class="w-50 mb-2"/>
-                             <p class="card-title fs-5">Create Account</p>
+                    <div class="col-12 col-md-6 col-lg-4 mb-3">
+                        <div class="card custom-card border rounded shadow p-3 lift-hover">
+                            <div class="card-body d-flex flex-column align-items-center">
+                                <img src="./public/images/notes.png" class="w-50 mb-2"/>
+                                <p class="card-title fs-5">My Tasks </p>
+                            </div>
+                            <a href="#" class="btn custom-primary-btn">Open</a>
                         </div>
-                        <a href="createAccount.html" class="btn custom-primary-btn">Open</a>
                     </div>
-                </div>
+                    <div class="col-12 col-md-6 col-lg-4 mb-3">
+                        <div class="card custom-card border rounded shadow p-3 lift-hover">
+                            <div class="card-body d-flex flex-column align-items-center">
+                                <img src="./public/images/teamwork.png" class="w-50 mb-2"/>
+                                <p class="card-title fs-5">Manage Group Tasks</p>
+                            </div>
+                            <a href="#" class="btn custom-primary-btn">Open</a>
+                        </div>
+                    </div>     
+                <?php endif; ?>
+
+
+                <?php if ($user["role"] === "project_manager"): ?>
+                    <div class="col-12 col-md-6 col-lg-4 mb-3">
+                        <div class="card custom-card border rounded shadow p-3 lift-hover">
+                            <div class="card-body d-flex flex-column align-items-center">
+                                <img src="./public/images/scope.png" class="w-50 mb-2"/>
+                                <p class="card-title fs-5">Projects</p>
+                            </div>
+                            <a href="showProjects.html" class="btn custom-primary-btn">Open</a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4 mb-3">
+                        <div class="card custom-card border rounded shadow p-3 lift-hover">
+                            <div class="card-body d-flex flex-column align-items-center">
+                                <img src="./public/images/choosing.png" class="w-50 mb-2"/>
+                                <p class="card-title fs-5">Manage Members</p>
+                            </div>
+                            <a href="#" class="btn custom-primary-btn">Open</a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4 mb-3">
+                        <div class="card custom-card border rounded shadow p-3 lift-hover">
+                            <div class="card-body d-flex flex-column align-items-center">
+                                <img src="./public/images/documentation.png" class="w-50 mb-2"/>
+                                <p class="card-title fs-5">Manage Tasks</p>
+                            </div>
+                            <a href="#" class="btn custom-primary-btn">Open</a>
+                        </div>
+                    </div>     
+                    <div class="col-12 col-md-6 col-lg-4 mb-3">
+                        <div class="card custom-card border rounded shadow p-3 lift-hover">
+                            <div class="card-body d-flex flex-column align-items-center">
+                                <img src="./public/images/clipboard.png" class="w-50 mb-2"/>
+                                <p class="card-title fs-5">Create Projects</p>
+                            </div>
+                            <a href="createProject.html" class="btn custom-primary-btn">Open</a>
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+
+                <?php if ($user["role"] === "admin"): ?>
+                    <div class="col-12 col-md-6 col-lg-4 mb-3">
+                        <div class="card custom-card border rounded shadow p-3 lift-hover">
+                            <div class="card-body d-flex flex-column align-items-center">
+                                <img src="./public/images/scope.png" class="w-50 mb-2"/>
+                                <p class="card-title fs-5">Projects</p>
+                            </div>
+                            <a href="showProjects.html" class="btn custom-primary-btn">Open</a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4 mb-3">
+                        <div class="card custom-card border rounded shadow p-3 lift-hover">
+                            <div class="card-body d-flex flex-column align-items-center">
+                                <img src="./public/images/choosing.png" class="w-50 mb-2"/>
+                                <p class="card-title fs-5">Members</p>
+                            </div>
+                            <a href="#" class="btn custom-primary-btn">Open</a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4 mb-3">
+                        <div class="card custom-card border rounded shadow p-3 lift-hover">
+                            <div class="card-body d-flex flex-column align-items-center">
+                                <img src="./public/images/documentation.png" class="w-50 mb-2"/>
+                                <p class="card-title fs-5">Tasks</p>
+                            </div>
+                            <a href="#" class="btn custom-primary-btn">Open</a>
+                        </div>
+                    </div>
+
+                
+                    <div class="col-12 col-md-6 col-lg-4 mb-3">
+                        <div class="card custom-card border rounded shadow p-3 lift-hover">
+                            <div class="card-body d-flex flex-column align-items-center">
+                                <img src="./public/images/clipboard.png" class="w-50 mb-2"/>
+                                <p class="card-title fs-5">Create Projects</p>
+                            </div>
+                            <a href="createProject.html" class="btn custom-primary-btn">Open</a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4 mb-3">
+                        <div class="card custom-card border rounded shadow p-3 lift-hover">
+                            <div class="card-body d-flex flex-column align-items-center">
+                                <img src="./public/images/add-friend.png" class="w-50 mb-2"/>
+                                <p class="card-title fs-5">Create Account</p>
+                            </div>
+                            <a href="createAccount.html" class="btn custom-primary-btn">Open</a>
+                        </div>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
    </div>
