85370387e284ae1cb5ee841728432502c3089b00
Anselmo Rivera <rivera.anselmo.2001@gmail.com>
2025-06-17 04:57:36 +0800

    added some validation in the task deadline cannot select data yesterday or today

--- src/Controllers/TaskController.php
+++ src/Controllers/TaskController.php
@@ -6,6 +6,8 @@ use App\Controllers\AbstractController;
 use App\Support\ProjectPanelService;
 use App\Support\SessionService;
 
+use DateTime;
+
 class TaskController extends AbstractController{
 
     public function __construct(private ProjectPanelService $projectPanelService) {}
@@ -35,6 +37,18 @@ class TaskController extends AbstractController{
 
         if (empty($taskDeadline)) {
             $errors["taskDeadlineErr"] = "Please enter the task deadline";
+        }
+
+        if (!empty($taskDeadline)) {
+            $deadlineDate = new DateTime($taskDeadline); 
+            $today = new DateTime();                        
+            
+            $deadlineStr = $deadlineDate->format('Y-m-d');
+            $todayStr = $today->format('Y-m-d');
+
+            if ($deadlineStr <= $todayStr) {
+                $errors["taskDeadlineErr"] = "Date cannot be set on the previous day or today.";
+            }
         }
 
         if (empty($taskType)) {
@@ -69,8 +83,24 @@ class TaskController extends AbstractController{
             exit;
         }
 
-        
+        $formData = [
+            "taskName" => $taskName,
+            "taskDescription" => $taskDescription,
+            "taskDeadline" => $taskDeadline,
+            "taskType" => $taskType,
+            "assignedMembers" => $assignedMembers,
+            "taskNote" => $taskNote
+        ];
 
+        $taskMeta = [
+            "projectId" => $projectId,
+            "isSubmitted" => 0,
+            "status" => "pending",
+            "approvalStatus" => NULL,
+        ];
+
+        $newTaskData = array_merge($formData, $taskMeta);
+    
     }
 
 }
